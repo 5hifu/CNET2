96ca82d08b04ff897f70ebcc5c4d37e30dde07a4
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-27 13:45:46 +0200

    osloveni pomoci dictionary

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -3,81 +3,34 @@
 List<string> jmena_list = ["Lukáš", "Karel", "Tomáš", 
             "Martin", "Radim", "Jakub", "Sofián"];
 
-jmena_list.Add("Petra");
-
-Console.WriteLine("Pocet lidi v seznamu: " + jmena_list.Count);
-
-foreach (var jmeno in jmena_list)
+Dictionary<string, string> jmena_osloveni = new Dictionary<string, string>()
 {
-    Console.WriteLine(jmeno + " pocet pismen: " + jmeno.Length);
-}
-
-
-Dictionary<int, string> dnyVTydnu = new Dictionary<int, string>()
-{
-    {1, "Pondělí"},
-    {2, "Úterý"},
-    {3, "Středa"},
-    {4, "Čtvrtek"},
-    {5, "Pátek"},
-    {6, "Sobota"},
-    {7, "Neděle"},
+    { "Lukáš", "Lukáši" },
+    { "Karel", "Karle" },
+    { "Tomáš", "Tomáš" },
+    { "Martin", "Martine" },
+    { "Radim", "Radime" },
+    { "Jakub", "Jakube" },
+    { "Sofián", "Sofiáne" },
 };
 
-var streda = dnyVTydnu[3];
-
-
-
-
-
-
-    
-
-
-//for(int i = 0; i < jmena.Length; i++)
-//{
-//    var jmeno = jmena[i];
-//    Console.WriteLine(jmeno);
-//}
-
-
-
-
-
-// Lukáš, Karel, Tomáš, Martin, Radim, Jakub, Sofián
 // zeptat se na jméno a pozdravit
 // nebo říct že neznám
 
-//Console.WriteLine("Ahoj, zadej své jméno:");
+Console.WriteLine("Ahoj, zadej své jméno:");
 
-//string jmeno = Console.ReadLine();
+string jmeno = Console.ReadLine();
 
-//switch (jmeno)
-//{
-//    case "Lukáš":
-//        Console.WriteLine("Ahoj Lukáši");
-//        break;
-//    case "Karel":
-//        Console.WriteLine("Ahoj Karle");
-//        break;
-//    case "Tomáš":
-//        Console.WriteLine("Ahoj Tomáši");
-//        break;
-//    case "Martin":
-//        Console.WriteLine("Ahoj Martine");
-//        break;
-//    case "Radim":
-//        Console.WriteLine("Ahoj Radime");
-//        break;
-//    case "Jakub":
-//        Console.WriteLine("Ahoj Jakube");
-//        break;
-//    case "Sofián":
-//        Console.WriteLine("Ahoj Sofiáne");
-//        break;
-//    default:
-//        Console.WriteLine("Takové jméno neznám.");
-//        break;
-//}
+if (!jmena_osloveni.ContainsKey(jmeno))
+{
+    Console.WriteLine("Takové jméno neznám.");
+    return;
+}
+
+var osloveni = jmena_osloveni[jmeno];
+
+Console.WriteLine($"Ahoj {osloveni}");
+
+
 
 
