159150e3f3ef8b77179134f89a8eb71a9081fa43
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-27 14:31:41 +0200

    LNNQ operatory

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -1,8 +1,38 @@
-﻿
+﻿using System.Linq;
 
 List<string> jmena_list = ["Lukáš", "Karel", "Tomáš",
             "Martin", "Radim", "Jakub", "Sofián"];
 
 
-var spojeno = string.Join(", ", jmena_list);
-Console.WriteLine(spojeno);
+int[] numbers = [13, 32335, -18, 0, 23, 233, 0, -12, 1235, -16000, 128933484];
+
+// WHERE - filtr
+var kladna = numbers
+            .Where(number => number > 0)
+            .ToList();
+
+// ORDER BY
+var ordered_desc = numbers
+            .OrderByDescending(number => number);
+
+// TAKE, SKIP
+var first5 = numbers.Take(5);
+var skip5 = numbers.Skip(5);
+
+// MAX, MIN, SUM, AVG - agregační
+var sum = numbers.Sum(number => number);
+var max = numbers.Max(number => number);
+
+Console.WriteLine($"sum: {sum}");
+Console.WriteLine($"max: {max}");
+
+// SELECT - transformace
+var positive = numbers
+        .Select(number => Math.Abs(number))
+        .Where(number => number > 1000);
+
+
+foreach (int number in positive)
+{
+    Console.WriteLine(number);
+}
