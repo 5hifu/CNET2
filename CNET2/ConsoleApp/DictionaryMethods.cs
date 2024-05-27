6ece14edcd493bfd9df86599f4c6febb80872f9b
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-27 13:52:57 +0200

    uklid dictionary

---
+++ CNET2/ConsoleApp/DictionaryMethods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class DictionaryMethods
+    {
+        public static void Dict()
+        {
+
+            List<string> jmena_list = ["Lukáš", "Karel", "Tomáš",
+            "Martin", "Radim", "Jakub", "Sofián"];
+
+            Dictionary<string, string> jmena_osloveni = new Dictionary<string, string>()
+            {
+                { "Lukáš", "Lukáši" },
+                { "Karel", "Karle" },
+                { "Tomáš", "Tomáš" },
+                { "Martin", "Martine" },
+                { "Radim", "Radime" },
+                { "Jakub", "Jakube" },
+                { "Sofián", "Sofiáne" },
+            };
+
+            // zeptat se na jméno a pozdravit
+            // nebo říct že neznám
+
+            Console.WriteLine("Ahoj, zadej své jméno:");
+
+            string jmeno = Console.ReadLine();
+
+            if (!jmena_osloveni.ContainsKey(jmeno))
+            {
+                Console.WriteLine("Takové jméno neznám.");
+                return;
+            }
+
+            var osloveni = jmena_osloveni[jmeno];
+
+            Console.WriteLine($"Ahoj {osloveni}");
+
+
+
+        }
+    }
+}
