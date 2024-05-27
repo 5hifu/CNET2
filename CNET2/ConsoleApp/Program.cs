b195d34aa792e5f0e16dcae8fb8ffbb0dd8c5f74
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-27 15:18:08 +0200

    reseni

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -6,6 +6,10 @@ var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 // vypište čísla v poli numbers jako slova
 
+var jako_slova = numbers.Select(num => strings[num]);
+Console.WriteLine($"jako slova: {string.Join(", ", jako_slova)}");
+
+
 
 
 
