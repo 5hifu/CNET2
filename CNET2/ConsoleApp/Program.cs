a7c260b9b8a93e49934025f2c86f0962282b04f8
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-27 14:33:43 +0200

    LINQ

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -8,7 +8,7 @@ int[] numbers = { 11, 2, 13, 97542, 44, -5, 6, 127, -99, 0, 256, 0, 12, 11 };
 // 4.  největší absolutní hodnotu
 // 5.  všechna kladná sudá čísla
 // 6.  seřaďte pole od nejmenších po největší hodnoty,
-//    přeskočte první 3 prvky a sečtěte zbytek hodnot
+// 7.   přeskočte první 3 prvky a sečtěte zbytek hodnot
 
 
 
