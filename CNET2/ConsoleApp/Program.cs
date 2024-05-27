ff5071dc8bdf3dc8c1ae7dedefb534a517409399
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-27 14:11:48 +0200

    string.join

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -2,3 +2,7 @@
 
 List<string> jmena_list = ["Lukáš", "Karel", "Tomáš",
             "Martin", "Radim", "Jakub", "Sofián"];
+
+
+var spojeno = string.Join(", ", jmena_list);
+Console.WriteLine(spojeno);
