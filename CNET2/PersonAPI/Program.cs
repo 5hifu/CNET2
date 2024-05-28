3a2cc8a1a530714ae35eedf87f7b09ac11c530bf
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-28 14:11:21 +0200

    PersonAPI /person/id

--- CNET2/PersonAPI/Program.cs
+++ CNET2/PersonAPI/Program.cs
@@ -1,6 +1,11 @@
+using PersonData;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<PeopleContext>();
+
 
 var app = builder.Build();
 
@@ -8,27 +13,10 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/person/{id}", (int id, PeopleContext db) =>
+                            db.People.Where(x => x.Id == id).Single());
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
