1f68c69774bd2e98f797f7e2961ca3fbbae01878
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-28 10:49:59 +0200

    GroupBy

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -2,15 +2,39 @@
 
 var data = DatasetAccess.LoadData(@"C:\Users\Student\source\repos\kubicek-skoleni\CNET2\data2024.json");
 
-var pocet_osob = data.Count();
+//var pocet_osob = data.Count();
 
-Console.WriteLine($"počet osob: {pocet_osob}");
+//Console.WriteLine($"počet osob: {pocet_osob}");
 
-var person = data.First();
-Console.WriteLine(person.Email);
+//var person = data.First();
+//Console.WriteLine(person.Email);
 
-var take1 = data.Take(1);
-foreach(var p in take1)
+//var take1 = data.Take(1).ToArray();
+//foreach (var p in take1)
+//{
+//    Console.WriteLine(p.Email);
+//}
+
+//var p2 = data.Where(person => person.Email.Contains("gmail.com"))
+//            .FirstOrDefault();
+
+//if (p2 != null)
+//{
+//    Console.WriteLine(p2.Email);
+//}
+//else
+//{
+//    Console.WriteLine("p2 is null");
+//}
+
+//data.Where(x => x.Id == 5).SingleOrDefault();
+
+var g = data.GroupBy(x => x.LastName);
+
+foreach(var skupina in g)
 {
-    Console.WriteLine(p.Email);
+    var lastname = skupina.Key;
+    var pocet_lidi_ve_skupine = skupina.Count();
+
+    Console.WriteLine($"{lastname}: {pocet_lidi_ve_skupine}");
 }
