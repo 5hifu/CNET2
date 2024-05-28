10beb67150ec8e6abb41f7af7faec461ce4de1de
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-28 12:52:46 +0200

    people context

--- CNET2/PersonData/Class1.cs
+++ CNET2/PersonData/Class1.cs
@@ -1,7 +1,19 @@
-﻿namespace PersonData
-{
-    public class Class1
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using PersonModel;
 
-    }
+namespace PersonData;
+
+public class PeopleContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    public DbSet<Address> Addresses { get; set; }
+
+    public DbSet<Contract> Contracts { get; set; }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite(@"Data Source=C:\Users\Student\source\repos\kubicek-skoleni\CNET2\CNET2\people.db");
 }
