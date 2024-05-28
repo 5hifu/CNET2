11ff7234af40bf745ee278393b5fad4b739ee86a
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-28 10:13:46 +0200

    deserializace json datasetu

---
+++ CNET2/PersonModel/DatasetAccess.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PersonModel
+{
+    internal class DatasetAccess
+    {
+        public static List<Person> LoadData(string filepath)
+        {
+            if (!File.Exists(filepath))
+            { 
+                throw new FileNotFoundException("Nenašel jsem dataset");
+            }
+
+            try
+            {
+                var filecontent = File.ReadAllText(filepath);
+                var people = JsonSerializer.Deserialize<List<Person>>(filecontent);
+                return people;
+            }
+            catch (Exception ex) 
+            {
+                // co s tím?
+                // Logger.Log(ex.Message)
+                Console.WriteLine("ERROR: " + ex.Message);
+                //return null;
+                throw ex;
+            }
+
+        }
+    }
+}
