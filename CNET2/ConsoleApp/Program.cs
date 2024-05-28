eccd978488d99b81276626d2481cd1d2f9bef07d
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-28 10:34:49 +0200

    take vs First

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -1,8 +1,16 @@
 ﻿using PersonModel;
 
-
 var data = DatasetAccess.LoadData(@"C:\Users\Student\source\repos\kubicek-skoleni\CNET2\data2024.json");
 
 var pocet_osob = data.Count();
 
 Console.WriteLine($"počet osob: {pocet_osob}");
+
+var person = data.First();
+Console.WriteLine(person.Email);
+
+var take1 = data.Take(1);
+foreach(var p in take1)
+{
+    Console.WriteLine(p.Email);
+}
