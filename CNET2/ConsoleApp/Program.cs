f0592e393059c1be6a416a2a0d536db413ef790c
kubicek-skoleni <kubicek.brno@gmail.com>
2024-05-28 14:59:04 +0200

    hledani osob dle emailu z konzole

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -5,11 +5,25 @@ using System.Net.Http.Json;
 var url = "https://localhost:7031";
 
 var client = new HttpClient();
-int id = 228;
+//int id = 228;
+//Person? p = await client.GetFromJsonAsync<Person>($"{url}/person/{id}");
+//Console.WriteLine(p);
 
-Person? p = await client.GetFromJsonAsync<Person>($"{url}/person/{id}");
+Console.WriteLine("Hledej podle emailu:");
+var email = Console.ReadLine();
+var res = await client.GetFromJsonAsync<List<Person>>($"{url}/person/searchemail/{email}");
 
-Console.WriteLine(p);
+if (res == null || res.Count == 0)
+{
+    Console.WriteLine("nenašel žádnou osobu");
+}
+else
+{
+    foreach (var item in res)
+    {
+        Console.WriteLine(item);
+    }
+}
 
 Console.ReadLine();
 
